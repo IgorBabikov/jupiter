0a7af04663f254989ee6f2821a2a0830d4f0f338
IgorBabikov <igorbabikov91@gmail.com>
2022-06-23 00:00:58 +0300

    added state for image to change activity

--- src/components/imageItem/ImageItem.jsx
+++ src/components/imageItem/ImageItem.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 import style from './imageItem.module.scss';
 
 function ImageItem({filter, title, img}) {
+  const [active, setActive] = useState(false)
+
+  const onClickImg = () => {
+    setActive(prev => !prev)
+  }
+
   return (
-    <div className={style.block}>
+    <div onClick={onClickImg} className={active ? style.active : style.block}>
        <img src={img} alt={title} />
        <button className={style.filter}>{filter}</button>
        <h3 className={style.title}>{title}</h3>
