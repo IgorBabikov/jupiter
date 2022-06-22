f219f9f48198c10b5a9beeb6ce06bd2703227973
IgorBabikov <igorbabikov91@gmail.com>
2022-06-23 01:42:18 +0300

    added tracking of the active filter

--- src/components/imageItem/ImageItem.jsx
+++ src/components/imageItem/ImageItem.jsx
@@ -1,18 +1,26 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setSortCategory } from '../../redux/slices/sortSlice';
 
 import style from './imageItem.module.scss';
 
 function ImageItem({filter, title, img}) {
   const [active, setActive] = useState(false)
 
+  const dispatch = useDispatch()
+
   const onClickImg = () => {
     setActive(prev => !prev)
+  }
+
+  const onClickShowFilter = () => {
+    dispatch(setSortCategory(filter))
   }
 
   return (
     <div onClick={onClickImg} className={active ? style.active : style.block}>
        <img src={img} alt={title} />
-       <button className={style.filter}>{filter}</button>
+       <button onClick={onClickShowFilter} className={style.filter}>{filter}</button>
        <h3 className={style.title}>{title}</h3>
     </div>
   )
