3cd16e5137daa63e54f6831bebf358506e3e9c8b
IgorBabikov <igorbabikov91@gmail.com>
2022-06-23 04:40:56 +0300

    added ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
+import ErrorBoundary from '../src/components/errorBoundary/ErrorBoundary'
 
 function App() {
   return (
     <div className="app">
       <Header/>
-      <Main/>
+      
+      <ErrorBoundary>
+         <Main/>
+      </ErrorBoundary>
     </div>
   );
 }
